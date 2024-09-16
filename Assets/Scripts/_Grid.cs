d8c5fbda938efc17ed6816c46b510f4ccddbf363
Emma <ewigur@gmail.com>
2024-09-16 16:27:16 +0200

    Code Cleanup, added bools

--- Assets/Scripts/_Grid.cs
+++ Assets/Scripts/_Grid.cs
@@ -9,22 +9,33 @@ public class _Grid : MonoBehaviour
     public float CellScale = 1.5f;
 
     [Header("Speed")]
-    public float SpeedOfSimulation = 4f;
+    public int SpeedOfSimulation = 4;
 
     [Header("Patterns of Life")]
     public bool RandomPatterns = false;
-  
+    public bool DrawPatterns = false;
+    
+    public bool started;
+
+
     void Start()
     {
         Application.targetFrameRate = 1; 
         GridCreation();
-        CellCheck();
+        //CellCheck();
     }
 
     private void Update()
     {
-        Application.targetFrameRate = (int)SpeedOfSimulation;
+        
+        if(!started)
+        {
+            return;
+        }
+
+        Application.targetFrameRate = SpeedOfSimulation;
         NeighborCheck();
+        
     }
 
     public void GridCreation()
@@ -56,9 +67,7 @@ public class _Grid : MonoBehaviour
 
                 SpriteRenderer spriteRenderer = cellObject.AddComponent<SpriteRenderer>();
 
-                spriteRenderer.sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-
-                spriteRenderer.color = Color.blue;                
+                spriteRenderer.sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f));               
 
                 float scaleX = CellSizeX / texture.width * CellScale;
                 float scaleY = CellSizeY / texture.height * CellScale;
@@ -71,70 +80,23 @@ public class _Grid : MonoBehaviour
                 float posY = CellStartY + (y * CellSizeY);
 
                 cellObject.transform.position = new Vector3(posX, posY, 0);
+
+                if (RandomPatterns)
+                {
+                    RandomSpawn(x, y);
+                }
+
+                if(DrawPatterns)
+                {
+                    DrawSpawn(x, y);
+                }
+                    
             }
         }        
     }
 
-    public void CellCheck()
-    {
-        for (int x = 0; x < CellRows; x++)
-        {
-            for (int y = 0; y < CellColumns; y++)
-            {
-
-                if (x + 1 < CellRows && myCells[x + 1, y] != null)
-                {
-                    LiveOrDead(x + 1, y);
-                }
-
-
-                if (y + 1 < CellColumns && myCells[x, y + 1] != null)
-                {
-                    LiveOrDead(x, y + 1);
-                }
-
-
-                if (x + 1 < CellRows && y + 1 < CellColumns && myCells[x + 1, y + 1] != null)
-                {
-                    LiveOrDead(x + 1, y + 1);
-                }
-
-
-                if (x - 1 >= 0 && myCells[x - 1, y] != null)
-                {
-                    LiveOrDead(x - 1, y);
-                }
-
-
-                if (y - 1 >= 0 && myCells[x, y - 1] != null)
-                {
-                    LiveOrDead(x, y - 1);
-                }
-
-
-                if (x - 1 >= 0 && y - 1 >= 0 && myCells[x - 1, y - 1] != null)
-                {
-                    LiveOrDead(x - 1, y - 1);
-                }
-
-
-                if (x - 1 >= 0 && y + 1 < CellColumns && myCells[x - 1, y + 1] != null)
-                {
-                    LiveOrDead(x - 1, y + 1);
-                }
-
-
-                if (x + 1 < CellRows && y - 1 >= 0 && myCells[x + 1, y - 1] != null)
-                {
-                    LiveOrDead(x + 1, y - 1);
-                }
-            }
-        }
-    }
-
     private void NeighborCheck()
     {
-
         bool[,] NextGen = new bool[CellRows, CellColumns];
 
         for (int x = 0; x < CellRows; x++)
@@ -166,16 +128,14 @@ public class _Grid : MonoBehaviour
                 {
                     if (liveNeighborCount < 2 || liveNeighborCount > 3)
                     {
-                        NextGen[x, y] = false; 
+                        NextGen[x, y] = false;
                         
                     }
-
 
                     else
                     {
                         NextGen[x, y] = true;
-                        myCells[x, y].GetComponent<SpriteRenderer>().color = Color.blue;
-
+                        myCells[x, y].GetComponent<SpriteRenderer>().color = Color.cyan; //Keeps on going strong
                     }
                 }
 
@@ -184,21 +144,18 @@ public class _Grid : MonoBehaviour
                     if (liveNeighborCount == 3)
                     {
                         NextGen[x, y] = true;
-                        myCells[x, y].GetComponent<SpriteRenderer>().color = Color.cyan;
+                        myCells[x, y].GetComponent<SpriteRenderer>().color = Color.blue; //Will probably die soon...
                     }
 
                     else
                     {
-                        NextGen[x, y] = false;
-                        
-
-                        
+                        NextGen[x, y] = false;                       
                     }
                 }
             }
         }
 
-
+        //Buffer
         for (int x = 0; x < CellRows; x++)
         {
             for (int y = 0; y < CellColumns; y++)
@@ -208,19 +165,6 @@ public class _Grid : MonoBehaviour
                     myCells[x, y].SetActive(NextGen[x, y]);
                 }
             }
-        }
-    }
-
-    public void LiveOrDead(int x, int y)
-    {
-        if(RandomPatterns)
-        {
-            RandomSpawn(x, y);
-        }
-        else
-        {
-            
-            myCells[x, y].SetActive(false);
         }
     }
 
@@ -234,14 +178,96 @@ public class _Grid : MonoBehaviour
         {
             myCells[x, y].SetActive(true);            
         }
-
         else
         {
             myCells[x, y].SetActive(false);            
         }
     }
 
+    private void DrawSpawn(int x, int y)
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            myCells[x, y].SetActive(true);  
+        }
+
+    }
+
+    /* Redundant Code
+     * 
+     *     public void LiveOrDead(int x, int y)
+    {
+        if(RandomPatterns)
+        {
+            RandomSpawn(x, y);
+        }
+        else
+        {
+            
+            myCells[x, y].SetActive(false);
+        }
+    }
+     * 
+       public void CellCheck()
+    {
+        for (int x = 0; x < CellRows; x++)
+        {
+            for (int y = 0; y < CellColumns; y++) //(Removed nullcheck (!= null) at the end of every if -statement)
+            {
+
+                if (x + 1 < CellRows && myCells[x + 1, y])
+                {
+                    LiveOrDead(x + 1, y);
+                }
+
+
+                if (y + 1 < CellColumns && myCells[x, y + 1])
+                {
+                    LiveOrDead(x, y + 1);
+                }
+
+
+                if (x + 1 < CellRows && y + 1 < CellColumns && myCells[x + 1, y + 1])
+                {
+                    LiveOrDead(x + 1, y + 1);
+                }
+
+
+                if (x - 1 >= 0 && myCells[x - 1, y])
+                {
+                    LiveOrDead(x - 1, y);
+                }
+
+
+                if (y - 1 >= 0 && myCells[x, y - 1])
+                {
+                    LiveOrDead(x, y - 1);
+                }
+
+
+                if (x - 1 >= 0 && y - 1 >= 0 && myCells[x - 1, y - 1])
+                {
+                    LiveOrDead(x - 1, y - 1);
+                }
+
+
+                if (x - 1 >= 0 && y + 1 < CellColumns && myCells[x - 1, y + 1])
+                {
+                    LiveOrDead(x - 1, y + 1);
+                }
+
+
+                if (x + 1 < CellRows && y - 1 >= 0 && myCells[x + 1, y - 1])
+                {
+                    LiveOrDead(x + 1, y - 1);
+                }
+            }
+        }
+    }
+     
+     */
 
 }
 
