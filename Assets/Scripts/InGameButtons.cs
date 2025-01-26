edfe28172ed5d8a497525f3950c1210e34b9a6b4
Emma <ewigur@gmail.com>
2025-01-26 23:57:07 +0100

    SpawnScene.Update

--- Assets/Scripts/InGameButtons.cs
+++ Assets/Scripts/InGameButtons.cs
@@ -9,10 +9,24 @@ namespace GameOfLife
     public class InGameButtons : MonoBehaviour
     {
         public GameObject Button;
+        public GameObject controlsPanel;
 
         public void RestartGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void ControlsButton()
+        {
+            if(controlsPanel != null)
+                controlsPanel.SetActive(true);
+        }
+
+        public void BackButton()
+        {
+            if(controlsPanel != null)
+                controlsPanel.SetActive(false);
+            
         }
 
         public void QuitToMenu()
--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -5,12 +5,28 @@ public class SliderScript : MonoBehaviour
     [SerializeField] private UnityEngine.UI.Slider slider;
     void Start()
     {
-        Application.targetFrameRate = 20;
+        Application.targetFrameRate = 10;
         slider.enabled = true;
+        slider.value = 10;
     }
 
-    void Update()
+    private void Update()
     {
-        Application.targetFrameRate = (int)slider.value;
+        ChangeValue();
+    }
+
+    public void ChangeValue()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && slider.value < slider.maxValue)
+        {
+            slider.value += 10;
+            Application.targetFrameRate = (int)slider.value;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && slider.value > slider.minValue)
+        {
+            slider.value -= 10;
+            Application.targetFrameRate = (int)slider.value;
+        }
     }
 }
